89b08a8cd23a241c9b5d614be7dbff12563cf51d
AlexeyLavrinovich <lavrinleha95@gmail.com>
2024-09-03 18:39:01 +0300

    Init commit

---
+++ twit/src/main/java/com/education/twit/TwitApplication.java
@@ -0,0 +1,13 @@
+package com.education.twit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitApplication.class, args);
+	}
+
+}
